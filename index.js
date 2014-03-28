708410363c8e897138a63a74976a020b2da308fa
YiChenshu <ycswaves@gmail.com>
2014-03-28 22:46:44 +0800

    fix indentation

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = function(obj){
-	var constructor = (obj.initialize === undefined)?
-			function(){} : obj.initialize;
-	for(var method in obj){
-		if (method !== 'initialize') {
-			constructor.prototype[method] = obj[method];
-		};
-	}
+  var constructor = (obj.initialize === undefined)?
+    function(){} : obj.initialize;
+  for(var method in obj){
+    if (method !== 'initialize') {
+      constructor.prototype[method] = obj[method];
+    };
+  }
   return constructor;
 };
 
