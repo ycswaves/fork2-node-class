4d70d5e963277f62c6908d59affdbede556e1e33
YiChenshu <ycswaves@gmail.com>
2014-03-30 19:27:50 +0800

    naive-super

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ module.exports = function(obj, parent) {
 
   //implement inheritance
   if (typeof parent !== 'undefined') {
+
+    //implement inheritance
     constructor.prototype = new parent();
     constructor.prototype.constructor = constructor;
     constructor.prototype.constructor.__super__ = parent;
+
+    //implement 'super'
+    constructor.prototype.super = function() {
+      var argsArr = Array.prototype.slice.call(arguments, 1);
+
+      if(typeof parent.prototype[arguments[0]] === 'function'){
+        return parent.prototype[arguments[0]].apply(this, argsArr);
+      }
+    }
   } else {
     constructor.__super__ = Object;
   }
@@ -16,6 +27,10 @@ module.exports = function(obj, parent) {
       constructor.prototype[method] = obj[method];
     }
   }
+
+
+
+
 
   return constructor;
 };
