f3c9bfd5869bed3c9a95d8a74ee178238c187994
YiChenshu <ycswaves@gmail.com>
2014-03-30 14:44:15 +0800

    method-inheritance

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-module.exports = function(obj){
+module.exports = function(obj, parent) {
   var constructor = (typeof obj.initialize === 'undefined')?
-    function(){} : obj.initialize;
-  for(var method in obj){
+                    function(){} : obj.initialize;
+
+  if (typeof parent !== 'undefined') {
+    constructor.prototype = new parent();
+    constructor.prototype.constructor = constructor;
+  }
+
+  for (var method in obj) {
     if (method !== 'initialize') {
       constructor.prototype[method] = obj[method];
-    };
+    }
   }
+
   return constructor;
 };
 
