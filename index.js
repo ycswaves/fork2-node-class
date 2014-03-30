b21bbe9fb43a6ebcc8d351a0d3154e1eb4b0f08b
YiChenshu <ycswaves@gmail.com>
2014-03-30 15:36:21 +0800

    super-class

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ module.exports = function(obj, parent) {
   var constructor = (typeof obj.initialize === 'undefined')?
                     function(){} : obj.initialize;
 
+  //implement inheritance
   if (typeof parent !== 'undefined') {
     constructor.prototype = new parent();
     constructor.prototype.constructor = constructor;
+    constructor.prototype.constructor.__super__ = parent;
+  } else {
+    constructor.__super__ = Object;
   }
 
   for (var method in obj) {
